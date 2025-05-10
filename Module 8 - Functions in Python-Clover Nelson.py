c2bd9fcf75d250f774236dd2748a71b7dfb3db2b
CloverNel <anelson274@ivytech.edu>
2025-05-10 16:43:07 -0400

    Add files via upload

---
+++ Module 8 - Functions in Python-Clover Nelson.py
@@ -0,0 +1,9 @@
+def greaterthan(x, y):
+    if x>y:
+        return "true"
+    else:
+        return "false"
+a=10
+b=6
+result = greaterthan(a, b)
+print("The statement " + str(a) + " is greater than " + str(b) + " is " + str(result))
